b5d1bcbbcfffc1404348e95ac09c7bac6985c89b
yCodeTech <studocwho@hotmail.com>
2026-01-17 04:07:54 +0000

    refactor: merge `createExtensionData` into `setExtensionData` method
    - Merge `ExtensionData::createExtensionData` method into `ExtensionData::setExtensionData` method for simplicity.
    
    We once had a unified method before until I split them into the 2 aforementioned methods in PR #12 (commit b0a40aa). Not sure why they were split, maybe because I thought too many `this.extensionData.set` was duplication, but it actually isn't. So we now merge them back.

--- src/extensionData.ts
+++ src/extensionData.ts
@@ -52,42 +52,26 @@ export class ExtensionData {
 	 * Set the extension data into the extensionData Map.
 	 */
 	private setExtensionData() {
-		// Set all entries in the extensionData Map.
-		Object.entries(this.createExtensionData()).forEach(([key, value]) => {
-			this.extensionData.set(key, value);
-		});
-	}
-
-	/**
-	 * Create the extension data object for the extensionData Map.
-	 * It also helps for type inference intellisense in the get method.
-	 *
-	 * @returns The extension data object with keys and values.
-	 */
-	private createExtensionData() {
 		// The path to the user extensions.
 		const userExtensionsPath = isWsl
 			? path.join(vscode.env.appRoot, "../../", "extensions")
 			: path.join(this.packageJsonData.extensionPath, "../");
 
-		// Set the keys and values for the Map.
-		// The keys will also be used for type inference in VSCode intellisense.
-		return {
-			id: this.packageJsonData.id,
-			name: this.packageJsonData.contributes.configuration.namespace,
-			displayName: this.packageJsonData.displayName,
-			version: this.packageJsonData.version,
-			userExtensionsPath: userExtensionsPath,
-			// The path to the built-in extensions.
-			// This env variable changes when on WSL to it's WSL-built-in extensions path.
-			builtInExtensionsPath: path.join(vscode.env.appRoot, "extensions"),
+		// Set each key-value pair directly into the Map
+		this.extensionData.set("id", this.packageJsonData.id);
+		this.extensionData.set("name", this.packageJsonData.contributes.configuration.namespace);
+		this.extensionData.set("displayName", this.packageJsonData.displayName);
+		this.extensionData.set("version", this.packageJsonData.version);
+		this.extensionData.set("userExtensionsPath", userExtensionsPath);
+		// The path to the built-in extensions.
+		// This env variable changes when on WSL to it's WSL-built-in extensions path.
+		this.extensionData.set("builtInExtensionsPath", path.join(vscode.env.appRoot, "extensions"));
 
-			// Only set these if running in WSL.
-			...(isWsl && {
-				WindowsUserExtensionsPathFromWsl: path.dirname(process.env.VSCODE_WSL_EXT_LOCATION!),
-				WindowsBuiltInExtensionsPathFromWsl: path.join(process.env.VSCODE_CWD!, "resources/app/extensions"),
-			}),
-		} as const;
+		// Only set these if running in WSL
+		if (isWsl) {
+			this.extensionData.set("WindowsUserExtensionsPathFromWsl", path.dirname(process.env.VSCODE_WSL_EXT_LOCATION!));
+			this.extensionData.set("WindowsBuiltInExtensionsPathFromWsl", path.join(process.env.VSCODE_CWD!, "resources/app/extensions"));
+		}
 	}
 
 	/**
